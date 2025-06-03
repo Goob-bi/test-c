9a760b8fc77777e9c14482aefa3a899e05ff23a7
Goob <goob@goob.gay>
2025-06-02 18:57:09 -0700

    we readin exact

--- test.c
+++ test.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-char* readexact(char* buff, int len) {
-    // add 1 for null terminator
-    return fgets(buff, len+1, stdin);
+#include <stdlib.h>
+
+char* readexact(size_t len) {
+    char *buff = malloc(len + 1);
+    if (!buff) return NULL;
+
+    if (fgets(buff, len + 1, stdin) == NULL) {
+        free(buff);
+        return NULL;
+    }
+
+    return buff;
 }
 
 void main(int argc, char **argv, char **envp) {
-    char test[10];
+    char* test;
     printf("hello dees:\n");
-    readexact(test, 6);
-    printf("%s", test);
+    test = readexact(5);
+    printf("%s: wow", test);
 }
